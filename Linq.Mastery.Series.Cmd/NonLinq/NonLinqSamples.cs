c0e69b5c37233fb128b41fc93d64a8797b9be0a4
Ahmad Sabbagh <engahmad.sabag1998@gmail.com>
2025-03-22 15:53:44 +0400

    Adding Non Linq Samples

---
+++ Linq.Mastery.Series.Cmd/NonLinq/NonLinqSamples.cs
@@ -0,0 +1,73 @@
+﻿using Linq.Mastery.Series.Data.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Linq.Mastery.Series.Cmd.NonLinq;
+
+public class NonLinqSamples
+{
+    public void Run()
+    {
+        // Working with in-memory objects
+        var objectsResult = new List<Book>();
+
+        var bookObjects = GetBooks();
+
+        foreach (var book in bookObjects)
+        {
+            if (book.Title.Contains("Agile"))
+            {
+                objectsResult.Add(book);
+            }
+        }
+
+        // Working with SQL databases
+        var sqlResult = new List<Book>();
+
+        var connection = new SqlConnection(GetConnectionString());
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = """
+                              SELECT Id, Name, PublishDate
+                              FROM dbo.Books
+                              WHERE Title LIKE '%Agile%'
+                              """;
+
+        var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            sqlResult.Add(new Book
+            {
+                BookId = (Guid)reader["Id"],
+                Title = (string)reader["Name"],
+                PublishDate = (DateOnly)reader["PublishDate"]
+            });
+        }
+
+        // Working with LINQ
+        var books = GetBooks();
+
+        var linqQueryResult =
+                from book in books
+                where book.Title.Contains("Agile")
+                select book;
+
+        var linqFluentResult = books
+            .Where(book => book.Title.Contains("Agile"))
+            .Select(book => book);
+
+        // Just to make the code highlighting look OK, and the results are used
+        Console.WriteLine(objectsResult.Count);
+        Console.WriteLine(sqlResult.Count);
+        Console.WriteLine(linqQueryResult.Count());
+        Console.WriteLine(linqFluentResult.Count());
+    }
+    List<Book> GetBooks()
+    {
+        return new List<Book>();
+    }
+    string GetConnectionString()
+    {
+        return string.Empty;
+    }
+}
---
+++ Linq.Mastery.Series.Cmd/QueryRunner.cs
@@ -0,0 +1,23 @@
+﻿using Linq.Mastery.Series.Data.Repositories;
+
+namespace Linq.Mastery.Series.Cmd;
+
+public abstract class QueryRunner
+{
+    protected readonly BookRepository Repository = new();
+
+    public abstract void Run();
+
+    protected void Print<T>(T objectToPrint)
+    {
+        Console.WriteLine(objectToPrint);
+    }
+
+    protected void PrintAll<T>(IEnumerable<T> objectsToPrint)
+    {
+        foreach (var objectToPrint in objectsToPrint)
+        {
+            Console.WriteLine(objectToPrint);
+        }
+    }
+}
