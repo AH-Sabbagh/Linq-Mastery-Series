ba4d6bcc6b6190277541c235987acd7a53d72ae4
Ahmad Sabbagh <engahmad.sabag1998@gmail.com>
2025-03-22 15:47:20 +0400

    Adding Models

---
+++ Linq.Mastery.Series.Data/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace Linq.Mastery.Series.Data.Modelsl;
+public class Book
+{
+    public required Guid BookId { get; init; }
+    public required string Title { get; init; }
+    public required DateOnly PublishDate { get; init; }
+    public int Edition { get; init; }
+    public List<Person> Authors { get; set; } = new List<Person>();
+    public List<Person> Editors { get; set; } = new List<Person>();
+
+    public override string ToString()
+    {
+        return $"[{BookId}] {Title} ({PublishDate.Year}) - {string.Join(", ", Authors)}";
+    }
+}
---
+++ Linq.Mastery.Series.Data/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Linq.Mastery.Series.Data.Models;
+
+public record Person(string Name, string Role)
+{
+    public override string ToString()
+    {
+        return $"{Name} {Role}";
+    }
+}
