09ef79619610ef619c76863d9901e86306b88377
Ahmad Sabbagh <engahmad.sabag1998@gmail.com>
2025-03-29 15:42:33 +0400

    Add "Ordring" Examples

---
+++ Linq.Mastery.Series.Cmd/2_FilteringAndOrdering/BookComparer.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Linq.Mastery.Series.Cmd._2_FilteringAndOrdering
+{
+    public class BookComparer : IComparer<Book>
+    {
+        public int Compare(Book? x, Book? y)
+        {
+            // Same instance
+            if (ReferenceEquals(x, y)) return 0;
+
+            // Null is smaller than everything
+            if (x is null) return -1;
+            if (y is null) return 1;
+
+            // If the years are different, sort by year
+            if (x.PublishDate.Year < y.PublishDate.Year)
+                return -1;
+            if (x.PublishDate.Year > y.PublishDate.Year)
+                return 1;
+
+            // If the years are equal, sort by name
+            return string.Compare(x.Title, y.Title, StringComparison.Ordinal);
+        }
+    }
+}
---
+++ Linq.Mastery.Series.Cmd/2_FilteringAndOrdering/Ordering.cs
@@ -0,0 +1,107 @@
+﻿namespace Linq.Mastery.Series.Cmd._2_FilteringAndOrdering;
+
+public class Ordering : QueryExecutor
+{
+    public override void Run()
+    {
+        SingleOrderBy_Q();
+    }
+
+    /// <summary>
+    /// Single order by, query syntax
+    /// </summary>
+    private void SingleOrderBy_Q()
+    {
+        var sourceBooks = Repository.GetAllBooks();
+
+        var result =
+            from book in sourceBooks
+            orderby book.Title
+            select book;
+
+        PrintAll(result);
+    }
+
+    /// <summary>
+    /// Single order by (descending), query syntax
+    /// </summary>
+    private void SingleOrderByDescending_Q()
+    {
+        var sourceBooks = Repository.GetAllBooks();
+
+        var result =
+            from book in sourceBooks
+            orderby book.Title descending
+            select book;
+
+        PrintAll(result);
+    }
+
+    /// <summary>
+    /// Single order by, fluent syntax
+    /// </summary>
+    private void SingleOrderBy_F()
+    {
+        var sourceBooks = Repository.GetAllBooks();
+
+        var result = sourceBooks
+            .OrderBy(book => book.Title);
+
+        PrintAll(result);
+    }
+
+    /// <summary>
+    /// Single order by (descending), fluent syntax
+    /// </summary>
+    private void SingleOrderByDescending_F()
+    {
+        var sourceBooks = Repository.GetAllBooks();
+
+        var result = sourceBooks
+            .OrderByDescending(book => book.Title);
+
+        PrintAll(result);
+    }
+
+    /// <summary>
+    /// Multiple order by, query syntax
+    /// </summary>
+    private void MultipleOrderBy_Q()
+    {
+        var sourceBooks = Repository.GetAllBooks();
+
+        var result =
+            from book in sourceBooks
+            orderby book.PublishDate.Year descending, book.Title
+            select book;
+
+        PrintAll(result);
+    }
+
+    /// <summary>
+    /// Multiple order by, fluent syntax
+    /// </summary>
+    private void MultipleOrderBy_F()
+    {
+        var sourceBooks = Repository.GetAllBooks();
+
+        var result = sourceBooks
+            .OrderByDescending(book => book.PublishDate.Year)
+            .ThenBy(book => book.Title);
+
+        PrintAll(result);
+    }
+
+    /// <summary>
+    /// Single order by using a custom comparer, fluent syntax
+    /// </summary>
+    private void OrderByCustomComparer_F()
+    {
+        var sourceBooks = Repository.GetAllBooks();
+
+        var result = sourceBooks
+            .OrderBy(book => book, new BookComparer());
+
+        PrintAll(result);
+    }
+}
